3b368a5f233849d9525fae6ffa10167e33b61035
Sainikhith <sainikhith98v@gmail.com>
2024-11-07 02:04:01 +0000

    updated

--- blog/views.py
+++ blog/views.py
@@ -29,13 +29,7 @@ def post_detail(request, id):
 #     return HttpResponse(f"Post Detail: {post}")  # Return a simple HttpResponse for demonstration
 #     # return render(request, 'blog/post_detail.html', {'post': post})
 
-# Create a view to delete a specific blog post based on its ID.
-# def post_delete(request, id):
-#     post = get_object_or_404(Post, pk=id)  # Fetch the post by ID or return a 404 error if not found
-#     if request.method == 'POST':
-#         post.delete()  # Delete the post from the database
-#         return redirect('post_list')  # Redirect to the list of posts after deletion
-#     return HttpResponse(f"Post Delete: {post}")
+
 
 
 def post_new(request):
