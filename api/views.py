093d589116c5e2999176ec31afb7a8cee303be05
Sainikhith <sainikhith98v@gmail.com>
2024-11-08 18:25:13 +0000

    added extra methods

--- api/views.py
+++ api/views.py
@@ -9,9 +9,41 @@ def getAllPosts(request):
     serializer = PostSerializer(posts, many=True)
     return Response(serializer.data)
 
+@api_view(['GET'])
+def getPostById(request, id):
+    try:
+        post = Post.objects.get(id=id)
+    except Post.DoesNotExist:
+        return Response({"error": "Post not found"}, status=404)
+    
+    serializer = PostSerializer(post)
+    return Response(serializer.data)
+
 @api_view(['POST'])
 def addPosts(request):
     serializer = PostSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
+
+@api_view(['PUT'])
+def updatePost(request, id):
+    try:
+        post = Post.objects.get(id=id)
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+    except Post.DoesNotExist:
+        return Response({"error": "Post not found"}, status=404)
+
+@api_view(['DELETE'])
+def deletePost(request, id):
+    try:
+        post = Post.objects.get(id=id)
+    except Post.DoesNotExist:
+        return Response({"error": "Post not found"}, status=404)
+    
+    post.delete()
+    return Response({"message": "Post deleted successfully"})
