f4f71b6c153c714a3a5e4f24a67cd13690c1364a
Sainikhith <sainikhith98v@gmail.com>
2024-11-08 18:41:28 +0000

    added swaggerui for endpoint management

--- blogapi/urls.py
+++ blogapi/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.getAllPosts),
     path('add/', views.addPosts),
-    path('<int:id>/', views.getPostById, name='get_post_by_id'),
+    path('get/<int:id>/', views.getPostById, name='get_post_by_id'),
     path('delete/<int:id>/', views.deletePost, name='delete_post'),
     path('update/<int:id>/', views.updatePost, name='update_post'),
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,8 +18,24 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+# Define the schema view
+schema_view = get_schema_view(
+   openapi.Info(
+      title="All Api's",
+      default_version='v1'
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     path('blog/', include('blog.urls')),
-    path('', include('api.urls'))
+    path('blogapi/', include('blogapi.urls')),
+    path('', schema_view.with_ui('swagger'), name='swagger-ui')
 ]
