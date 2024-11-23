e822acf618018e39e077fc765950ecb48b1d5266
Sainikhith <sainikhith98v@gmail.com>
2024-11-23 15:59:57 +0000

    added cors flexibility for react app

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,17 +41,23 @@ INSTALLED_APPS = [
     'drf_yasg',
     'rest_framework',
     'rest_framework.authtoken',
-    'rest_framework_api_key'
+    'rest_framework_api_key',
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Your React app's URL
 ]
 
 ROOT_URLCONF = 'mysite.urls'
